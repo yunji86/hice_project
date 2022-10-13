65504181717cbfe8f144c803b8bf2b1c3c095c95
yunji86 <rhaehfdlvn91@gmail.com>
2022-10-13 16:30:31 +0900

    리뷰 게시판 파일 추가

---
+++ semi_project/src/main/java/com/greedy/coffee/file/dto/FileDTO.java
@@ -0,0 +1,19 @@
+package com.greedy.coffee.file.dto;
+
+
+
+import lombok.Data;
+@Data
+public class FileDTO {
+	private Long fileCode;
+	private String fileOrgName;
+	private String fileSaveName;
+	private String filePath;
+	private String fileStatus;
+	private String fileType;
+	private Long revCode;	
+	private String fileThumName;
+	private String fileThumPath;
+	//private ProDTO proCode;
+	
+}
