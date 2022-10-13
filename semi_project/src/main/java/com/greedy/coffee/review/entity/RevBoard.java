78c9a9bca1d9dbe02685f1debc0733ab31bbdd4e
yunji86 <rhaehfdlvn91@gmail.com>
2022-10-13 16:29:29 +0900

    리뷰 게시판

---
+++ semi_project/src/main/java/com/greedy/coffee/review/entity/RevBoard.java
@@ -0,0 +1,72 @@
+package com.greedy.coffee.review.entity;
+
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import com.greedy.coffee.file.entity.File;
+import com.greedy.coffee.member.entity.Member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@DynamicInsert
+@Entity
+@Table(name= "TBL_REVIEW")
+@SequenceGenerator(name = "REV_SEQ_GENERATOR", sequenceName = "SEQ_REV_CODE", initialValue = 1, allocationSize = 1)
+public class RevBoard {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "REV_SEQ_GENERATOR")
+	@Column(name = "REV_CODE")
+	private Long revCode;
+	
+	@Column(name = "REV_TITLE")
+	private String revTitle;
+	
+	@Column(name = "REV_CONTENTS")
+	private String revContents;
+	
+	@Column(name = "REV_DATE")
+	private Date revDate;
+	
+	@Column(name = "REV_EDIT_DATE")
+	private Date revEditDate;
+	
+	@Column(name = "REV_DELETE_DATE")
+	private Date revDeleteDate;
+	
+	@Column(name = "REV_STATUS")
+	private String revStatus;
+	
+	@ManyToOne
+	@JoinColumn(name="MEM_ID")
+	private Member writer;
+	
+	/*@ManyToOne
+	@JoinColumn(name="PRO_CODE")
+	private Product pro;*/
+	
+	@OneToMany(cascade=CascadeType.PERSIST)
+	@JoinColumn(name = "REV_CODE")
+	private List<File> fileList;
+	
+	
+}
---
+++ semi_project/src/main/java/com/greedy/coffee/review/repository/RevBoardRepository.java
@@ -0,0 +1,63 @@
+package com.greedy.coffee.review.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.greedy.coffee.like.entity.Like;
+import com.greedy.coffee.member.dto.MemberDTO;
+import com.greedy.coffee.member.entity.Member;
+import com.greedy.coffee.review.entity.RevBoard;
+
+
+public interface RevBoardRepository extends JpaRepository<RevBoard, Long> {
+
+	@Query("SELECT r "+
+			 "FROM RevBoard r " +
+			"WHERE r.revStatus = :revStatus " +
+			  "AND (r.revTitle LIKE '%' || :revSearchValue || '%' " +
+			  "OR r.revContents LIKE '%' || :revSearchValue || '%')")
+	Page<RevBoard> findByRevSearchValue(@Param("revStatus") String revStatus, @Param("revSearchValue") String revSearchValue, Pageable pagable);
+
+	RevBoard findByRevCode(Long revCode);
+
+	
+
+	Page<RevBoard> findByRevStatus(String acitveStatus, Pageable pagable);
+	
+	
+	RevBoard findByRevCodeAndRevStatus(Long revCode, String acitveStatus);
+
+	Like findByLoginIdAndLikeStatus(MemberDTO loginId, String acitveStatus);
+
+	
+	
+	
+
+	
+
+	
+
+	
+
+	
+
+
+
+
+	
+
+	
+
+
+
+
+	
+			  
+
+	
+
+}
