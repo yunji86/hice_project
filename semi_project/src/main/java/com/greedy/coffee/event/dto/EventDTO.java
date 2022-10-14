d7522d657ea6dd32ed0587154e0405cd36ec9a62
dongyoung Kim <tkrlek23@gmail.com>
2022-10-14 11:43:27 +0900

    event 파일 및 css + 이미지 추가

---
+++ semi_project/src/main/java/com/greedy/coffee/event/dto/EventDTO.java
@@ -0,0 +1,52 @@
+package com.greedy.coffee.event.dto;
+
+import java.sql.Date;
+import com.greedy.coffee.member.dto.MemberDTO;
+import com.greedy.coffee.member.entity.Member;
+
+import lombok.Data;
+
+@Data
+public class EventDTO {
+
+	private Long eveCode;
+	private String eveCompany;
+	private String eveDepartment;
+	private String evePosition;
+	private String eveSector;
+	private String eveRoute;
+	private String memId;
+	private Date eveDate;
+	
+	/*
+	 * public EventDTO() { super(); }
+	 * 
+	 * public EventDTO(Long eveCode, String eveCompany, String eveDepartment, String
+	 * evePosition, String eveSector, String eveRoute, MemberDTO memId, Date
+	 * eveDate) { super(); this.eveCode = eveCode; this.eveCompany = eveCompany;
+	 * this.eveDepartment = eveDepartment; this.evePosition = evePosition;
+	 * this.eveSector = eveSector; this.eveRoute = eveRoute; this.memId = memId;
+	 * this.eveDate = eveDate; }
+	 * 
+	 * public Long getEveCODE() { return eveCode; } public void setEveCODE(Long
+	 * eveCode) { this.eveCode = eveCode; } public String getEveCompany() { return
+	 * eveCompany; } public void setEveCompany(String eveCompany) { this.eveCompany
+	 * = eveCompany; } public String getEveDepartment() { return eveDepartment; }
+	 * public void setEveDepartment(String eveDepartment) { this.eveDepartment =
+	 * eveDepartment; } public String getEvePosition() { return evePosition; }
+	 * public void setEvePosition(String evePosition) { this.evePosition =
+	 * evePosition; } public String getEveSector() { return eveSector; } public void
+	 * setEveSector(String eveSector) { this.eveSector = eveSector; } public String
+	 * getEveRoute() { return eveRoute; } public void setEveRoute(String eveRoute) {
+	 * this.eveRoute = eveRoute; } public MemberDTO getMemId() { return memId; }
+	 * public void setMemId(MemberDTO memId) { this.memId = memId; } public Date
+	 * getEveDate() { return eveDate; } public void setEveDate(Date eveDate) {
+	 * this.eveDate = eveDate; }
+	 * 
+	 * @Override public String toString() { return "EventDTO [eveCode=" + eveCode +
+	 * ", eveCompany=" + eveCompany + ", eveDepartment=" + eveDepartment +
+	 * ", evePostion=" + evePosition + ", eveSector=" + eveSector + ", eveRoute=" +
+	 * eveRoute + ", memId=" + memId + ", eveDate=" + eveDate + "]"; }
+	 */
+	
+}
---
+++ semi_project/src/main/java/com/greedy/coffee/event/entity/Event.java
@@ -0,0 +1,61 @@
+package com.greedy.coffee.event.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+
+import com.greedy.coffee.member.dto.MemberDTO;
+import com.greedy.coffee.member.entity.Member;
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "TBL_EVENT")
+@SequenceGenerator(name = "EVE_SEQ_GENERATOR", sequenceName = "SEQ_EVE_CODE", initialValue = 1, allocationSize = 1)
+@DynamicInsert
+public class Event {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "EVE_SEQ_GENERATOR")
+	@Column(name = "EVE_CODE")
+	private Long eveCode;
+	
+	@Column(name = "EVE_COMPANY")
+	private String eveCompany;
+	
+	@Column(name = "EVE_DEPARTMENT")
+	private String eveDepartment;
+	
+	@Column(name = "EVE_POSITION")
+	private String evePosition;
+	
+	@Column(name = "EVE_SECTOR")
+	private String eveSector;
+	
+	@Column(name = "EVE_ROUTE")
+	private String eveRoute;
+	
+	@ManyToOne
+	@JoinColumn(name = "MEM_ID")
+	private Member memId;
+	
+	@Column(name = "EVE_DATE")
+	private Date eveDate;
+	
+}
